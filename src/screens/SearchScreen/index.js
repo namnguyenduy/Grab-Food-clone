e74335d853555ccfb8306f40d46d1a8cba7d89f2
nam <namsupermen06@gmail.com>
2022-11-12 18:04:56 +0700

    top filter and all filter search

--- src/screens/SearchScreen/index.js
+++ src/screens/SearchScreen/index.js
@@ -1,15 +1,93 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableWithoutFeedback,
+  ImageBackground,
+  Dimensions,
+  StyleSheet,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 
 import SearchComponent from "../../components/SearchComponent";
+import filterData from "../../assets/data/filterData.json";
+import { colors } from "../../global/styles";
 
+const SCREEN_WIDTH = Dimensions.get("window").width;
 const SearchScreen = () => {
+  const listFilterRender = ({ item }) => {
+    return (
+      <TouchableWithoutFeedback>
+        <View style={styles.imageView} className="rounded-md items-center justify-center">
+          <ImageBackground
+            className="rounded-md border bg-green-400 w-full h-full"
+            source={{ uri: item.image }}
+          >
+            <View
+              className="items-center justify-center w-full h-full"
+              style={{ backgroundColor: "rgba(52, 52, 52,0.3)" }}
+            >
+              <Text className="text-cardBackground text-lg">{item.name}</Text>
+            </View>
+          </ImageBackground>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  };
+
+  const Footer = () => {
+    return (
+      <View className="my-5">
+        <FlatList
+          initialNumToRender={1}
+          data={filterData}
+          keyExtractor={(item) => item.id}
+          renderItem={listFilterRender}
+          horizontal={false}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          ListHeaderComponent={<Text style={styles.listHeader}>Tất cả danh mục</Text>}
+        />
+      </View>
+    );
+  };
+
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1">
       <SearchComponent />
+      <View className="my-5">
+        <FlatList
+          contentContainerStyle={{ paddingBottom: 20 }}
+          initialNumToRender={1}
+          data={filterData}
+          keyExtractor={(item) => item.id}
+          renderItem={listFilterRender}
+          horizontal={false}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          ListHeaderComponent={<Text style={styles.listHeader}>Top danh mục</Text>}
+          ListFooterComponent={<Footer />}
+        />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default SearchScreen;
+
+const styles = StyleSheet.create({
+  imageView: {
+    width: SCREEN_WIDTH * 0.4475,
+    height: SCREEN_WIDTH * 0.4475,
+    marginLeft: SCREEN_WIDTH * 0.035,
+    marginBottom: SCREEN_WIDTH * 0.035,
+  },
+  listHeader: {
+    fontSize: 18,
+    color: colors.grey1,
+    paddingBottom: 10,
+    marginLeft: 12,
+    fontWeight: "bold",
+  },
+});
